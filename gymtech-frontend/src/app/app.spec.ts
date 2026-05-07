5a4635abc2870830297f3de5ffbc2a33f18d8d9e
luidort-zsuzsa-30323 <luidort.zsuzsa@gmail.com>
2026-05-08 00:00:59 +0300

    test correction

--- gymtech-frontend/src/app/app.spec.ts
+++ gymtech-frontend/src/app/app.spec.ts
@@ -21,35 +21,6 @@ describe('App', () => {
     expect(compiled.querySelector('h1')?.textContent).toContain('Hello, gymtech-frontend');
   });
 
-  //tester
 
-  it('should have a username input field', () => {
-    const compiled = fixture.nativeElement;
-    const username = compiled.querySelector(ínput[type = "text"]);
-    expect(username).toBeTruthy();
-  });
-
-  it('should have a password input field', () => {
-    const compiled = fixture.nativeElement;
-    const Password = compiled.querySelector(ínput[type = "password"]);
-    expect(Password).toBeTruthy();
-  });
-
-  it('should have a Log In button', () => {
-    const compiled = fixture.nativeElement;
-    const loginButton = compiled.querySelector('button');
-    expect(loginButton).toBeTruthy();
-  });
-
-  it('should have a link to the sign-up page', () => {
-    const compiled = fixture.nativeElement;
-    const signUpLink = compiled.querySelector('a');
-    expect(signUpLink).toBeTruthy();
-
-    const linkPath = signUpLink.getAttribute('routerLink') || signUpLink.getAttribute('href');
-    expect(linkPath).toContain('/signUp')
-  });
-
-  //till here
 
 });
